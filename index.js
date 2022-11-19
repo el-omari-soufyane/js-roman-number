d6676ad5851936b1da0e66421ceecf721f7b13cb
DouniaN <nougaouidounia.dev@gmial.com>
2022-11-19 22:32:46 +0100

    Dounia - Refactoring VI to VIII

--- index.js
+++ index.js
@@ -5,9 +5,7 @@ class RomanConverter {
     if (arabNumber < 4) return "I".repeat(arabNumber);
     else if (arabNumber == 4) return "IV";
     else if (arabNumber == 5) return "V";
-    else if (arabNumber == 6) return "VI";
-    else if (arabNumber == 7) return "VII";
-    else if (arabNumber == 8) return "VIII";
+    else if(arabNumber > 5 && arabNumber < 9) return "V" + "I".repeat(arabNumber - 5);
     return "IX";
   }
 }
