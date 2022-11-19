7a70b481bb85c2a00c9bbff0b87852bb6a1282ad
bachir1998 <gueyebachir98@gmail.com>
2022-11-20 00:27:31 +0100

    Bassirou - Refactoring XXI to XXIII

--- index.js
+++ index.js
@@ -5,17 +5,16 @@ class RomanConverter {
     if (arabNumber < 4) return "I".repeat(arabNumber);
     else if (arabNumber == 4) return "IV";
     else if (arabNumber == 5) return "V";
-    else if(arabNumber > 5 && arabNumber < 9) return "V" + "I".repeat(arabNumber - 5);
+    else if (arabNumber > 5 && arabNumber < 9) return "V" + "I".repeat(arabNumber - 5);
     else if (arabNumber == 9) return "IX";
     else if (arabNumber == 10) return "X"
     else if (arabNumber > 10 && arabNumber < 14) return "X" + "I".repeat(arabNumber - 10)
     else if (arabNumber == 14) return "XIV"
     else if (arabNumber == 15) return "XV";
-    else if(arabNumber > 15 && arabNumber <19) return "XV" + "I".repeat(arabNumber - 15);
+    else if (arabNumber > 15 && arabNumber < 19) return "XV" + "I".repeat(arabNumber - 15);
     else if (arabNumber == 19) return "XIX";
     else if (arabNumber == 20) return "XX";
-    else if (arabNumber == 21) return "XXI";
-    else if (arabNumber == 22) return "XXII";
+    else if (arabNumber > 20 && arabNumber < 24) return "XX" + "I".repeat(arabNumber - 20);
     return "XXIII";
   }
 }
