c35c86646b4e2c78908b5b0fb9d8908fb6c107b6
bachir1998 <gueyebachir98@gmail.com>
2022-11-20 01:21:09 +0100

    Bassirou - Refactoring addUnites method to accept arab number and the unit

--- index.js
+++ index.js
@@ -1,29 +1,30 @@
 class RomanConverter {
   constructor() { }
-  
+
   /**
    * @param {number} arabNumber
    */
-  addUnites(arabNumber) {
-    return "I".repeat(arabNumber);
+  addUnites(arabNumber, unit = "", preNumber = false) {
+    const repeated = "I".repeat(arabNumber);
+    return preNumber ? repeated + unit : unit + repeated;
   }
 
   convert(arabNumber) {
     if (arabNumber < 4) return this.addUnites(arabNumber);
     else if (arabNumber == 4) return "IV";
     else if (arabNumber == 5) return "V";
-    else if (arabNumber > 5 && arabNumber < 9) return "V" + this.addUnites(arabNumber - 5);
+    else if (arabNumber > 5 && arabNumber < 9) return this.addUnites(arabNumber - 5, "V");
     else if (arabNumber == 9) return "IX";
     else if (arabNumber == 10) return "X"
-    else if (arabNumber > 10 && arabNumber < 14) return "X" + this.addUnites(arabNumber - 10)
+    else if (arabNumber > 10 && arabNumber < 14) return this.addUnites(arabNumber - 10, "X")
     else if (arabNumber == 14) return "XIV"
     else if (arabNumber == 15) return "XV";
-    else if (arabNumber > 15 && arabNumber < 19) return "XV" + this.addUnites(arabNumber - 15);
+    else if (arabNumber > 15 && arabNumber < 19) return this.addUnites(arabNumber - 15, "XV");
     else if (arabNumber == 19) return "XIX";
     else if (arabNumber == 20) return "XX";
-    else if (arabNumber > 20 && arabNumber < 24) return "XX" + this.addUnites(arabNumber - 20);
+    else if (arabNumber > 20 && arabNumber < 24) return this.addUnites(arabNumber - 20, "XX");
     else if (arabNumber == 24) return "XXIV";
-    else if (arabNumber > 24 && arabNumber < 29) return "XXV" + this.addUnites(arabNumber - 25);
+    else if (arabNumber > 24 && arabNumber < 29) return this.addUnites(arabNumber - 25, "XXV");
     else if (arabNumber == 29) return "XXIX";
     return "XXX";
   }
